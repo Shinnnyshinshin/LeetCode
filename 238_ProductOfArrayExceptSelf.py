8a7b38a68b1e6a690dcfa617b1135bbe76864e17
Shinnnyshinshin <willshin@gmail.com>
2020-03-28 20:52:03 -0700

    Update 238_ProductOfArrayExceptSelf.py

--- 238_ProductOfArrayExceptSelf.py
+++ 238_ProductOfArrayExceptSelf.py
@@ -26,7 +26,15 @@ Could you solve it with constant space complexity? (The output array does not co
 #    Approach
 #-------------------------------------------------------------------------------
 
+"""
+    The simplest approach would be to multiply everything, and then fill the array with total / number at current index. But leetcode says don't do that
 
+    * So what would be the next way to do it? 
+    * Is it possible to have 2 more arrays, where the number
+    
+    - forward[]
+    - backward[]
+"""
 
 #-------------------------------------------------------------------------------
 #    Soluton
@@ -34,8 +42,40 @@ Could you solve it with constant space complexity? (The output array does not co
 
                 
 def my_productExceptSelf(nums):
-    # how do we go about doing this? 
-    pass
+
+    # The length of the input array
+    length = len(nums)
+
+    # The left and right arrays as described in the algorithm
+    L, R, answer = [0]*length, [0]*length, [0]*length
+
+    # L[i] contains the product of all the elements to the left
+    # Note: for the element at index '0', there are no elements to the left,
+    # so the L[0] would be 1
+    L[0] = 1
+    for i in range(1, length):
+        # L[i - 1] already contains the product of elements to the left of 'i - 1'
+        # Simply multiplying it with nums[i - 1] would give the product of all
+        # elements to the left of index 'i'
+        L[i] = nums[i - 1] * L[i - 1]
+    
+    # R[i] contains the product of all the elements to the right
+    # Note: for the element at index 'length - 1', there are no elements to the right,
+    # so the R[length - 1] would be 1
+    R[length - 1] = 1
+    for i in reversed(range(length - 1)):
+        # R[i + 1] already contains the product of elements to the right of 'i + 1'
+        # Simply multiplying it with nums[i + 1] would give the product of all
+        # elements to the right of index 'i'
+        R[i] = nums[i + 1] * R[i + 1]
+    # Constructing the answer array
+    for i in range(length):
+        # For the first element, R[i] would be product except self
+        # For the last element of the array, product except self would be L[i]
+        # Else, multiple product of all elements to the left and to the right
+        answer[i] = L[i] * R[i]
+    return(answer)
+
 #-------------------------------------------------------------------------------
 #    Main Leetcode Input Driver
 #-------------------------------------------------------------------------------
